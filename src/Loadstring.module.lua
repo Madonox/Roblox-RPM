78697ac1da006a1ecd3a50a8f35500d2b67dcf63
Madonox <39967871+Madonox@users.noreply.github.com>
2021-09-25 13:19:21 -0400

    Create Loadstring.module.lua

---
+++ src/Loadstring.module.lua
@@ -0,0 +1,10 @@
+--[[
+	Credit to einsteinK.
+	Credit to Stravant for LBI.
+	
+	Credit to the creators of all the other modules used in this.
+	
+	JAILBREAKFAN12313 was here and decided modify some things.
+	
+	einsteinK was here again to fix a bug in LBI for if-statements
+--]]
